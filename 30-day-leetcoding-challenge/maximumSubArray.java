3d5bc7c92f9a02ded420dbe533c3144c46c3203f
OchirnyamB <ochirnyam@gmail.com>
2020-07-30 10:03:11 -0400

    Add solution for Maximum Subarray

---
+++ 30-day-leetcoding-challenge/maximumSubArray.java
@@ -0,0 +1,20 @@
+package Solved-Algorithm-Challenges.30-day-leetcoding-challenge;
+
+// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+// Example:
+
+// Input: [-2,1,-3,4,-1,2,1,-5,4],
+// Output: 6
+// Explanation: [4,-1,2,1] has the largest sum = 6.
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+       int maxSum = nums[0], sum = 0;
+       for(int i = 0; i < nums.length; i++){
+          sum = Math.max(nums[i], nums[i]+sum);
+          maxSum = Math.max(maxSum, sum);   
+       }
+       return maxSum;
+    }
+}
