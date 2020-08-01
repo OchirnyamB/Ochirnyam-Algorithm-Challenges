6f1a5032eae277a62941c2abf8d532448559a32b
OchirnyamB <ochirnyam@gmail.com>
2020-08-01 19:40:54 -0400

    Add solutions for Jump Game

--- 30-day-leetcoding-challenge/maximumSubArray.java
+++ 30-day-leetcoding-challenge/maximumSubArray.java
@@ -1,5 +1,3 @@
-package Solved-Algorithm-Challenges.30-day-leetcoding-challenge;
-
 // Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 
 // Example:
